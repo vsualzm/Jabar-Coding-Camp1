0a4be5d6e0922fb5c7fc870efa036a1ed73f5807
vsualzm <ims.ilham@gmail.com>
2021-09-05 01:40:43 +0700

    Soal 1

---
+++ quiz 1/script.js
@@ -0,0 +1,35 @@
+
+// 1. Judul : Function Penghitung Jumlah Kata
+// Buatlah sebuah function dengan nama jumlah_kata() yang menerima sebuah kalimat (string), dan mengembalikan nilai jumlah kata dalam kalimat tersebut.
+// Contoh
+
+// var kalimat_1 = " Halo nama saya Muhammad Iqbal Mubarok "
+// var kalimat_2 = "Saya Iqbal"
+// jumlah_kata(kalimat_1) // 6
+// jumlah_kata(kalimat_2) // 2
+
+function jumlahKata(num){
+  var pisah = num.split(' ').filter((kata) => {if (kata !== ' ')
+  return kata;
+  })
+ return pisah.length;
+}
+
+var kalimat = "Nama saya Ilham Maulana Sugianto";
+var hasil = jumlahKata(kalimat)
+console.log(kalimat + ' jumlah huruf' + ' = ' + hasil + '\n')
+
+
+// var tanggal = 30
+// var bulan = 8
+// var tahun = 2000
+
+// function tanggalBesok(tanggal, bulan, tahun){
+//   var bulan = ["Januari", "febuari", "maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "desember"];
+
+// }
+
+
+
+
+
