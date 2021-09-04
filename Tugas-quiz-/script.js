a1a4afbc95888f278c0adde81ba2b02f63132402
vsualzm <ims.ilham@gmail.com>
2021-09-05 02:15:12 +0700

    soal2

--- Tugas-quiz-/script.js
+++ Tugas-quiz-/script.js
@@ -19,6 +19,20 @@ var kalimat = "Nama saya Ilham Maulana Sugianto";
 var hasil = jumlahKata(kalimat)
 console.log(kalimat + ' jumlah huruf' + ' = ' + hasil + '\n')
 
+// tugas 2 
+// isi
+var tanggal = 15;
+var bulan = 6;
+var tahun = 2021;
 
-
+function tanggalBesok(tanggal, bulan, tahun){
+    var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Desember"];
+    var pilihBulan = months[bulan-1];
+    var target = pilihBulan + '/' + tanggal.toString() + '/' + tahun.toString();
+    var s = new Date(target);
+    s.setDate(s.getDate() +1);
+    return `${s.getDate()} ${months[s.getMonth()]} ${(s.getFullYear())}`;
+}
+var hasilNilai = tanggalBesok(tanggal, bulan, tahun);
+console.log(hasilNilai);
 
--- quiz/script.js
+++ quiz/script.js
@@ -1,12 +1,4 @@
 // soal 1
-// 1. Judul : Function Penghitung Jumlah Kata
-// Buatlah sebuah function dengan nama jumlah_kata() yang menerima sebuah kalimat (string), dan mengembalikan nilai jumlah kata dalam kalimat tersebut.
-// Contoh
-
-// var kalimat_1 = " Halo nama saya Muhammad Iqbal Mubarok "
-// var kalimat_2 = "Saya Iqbal"
-// jumlah_kata(kalimat_1) // 6
-// jumlah_kata(kalimat_2) // 2
 
 function jumlahKata(num){
   var pisah = num.split(' ').filter((kata) => {if (kata !== ' ')
@@ -20,5 +12,22 @@ var hasil = jumlahKata(kalimat)
 console.log(kalimat + ' jumlah huruf' + ' = ' + hasil + '\n')
 
 
+
+// soal 2 
+
+var tanggal = 15;
+var bulan = 6;
+var tahun = 2021;
+
+function tanggalBesok(tanggal, bulan, tahun){
+    var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Desember"];
+    var pilihBulan = months[bulan-1];
+    var target = pilihBulan + '/' + tanggal.toString() + '/' + tahun.toString();
+    var s = new Date(target);
+    s.setDate(s.getDate() +1);
+    return `${s.getDate()} ${months[s.getMonth()]} ${(s.getFullYear())}`;
+}
+var hasilNilai = tanggalBesok(tanggal, bulan, tahun);
+console.log(hasilNilai);
 
 
